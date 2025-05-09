c84298de29dda575509543c04e48e22e080707a1
Tolha127 <tolhanajmudeen2006@gmail.com>
2025-05-09 03:21:37 +0300

    Remove local configuration and build files from Git tracking

--- build-client.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set NODE_OPTIONS=--openssl-legacy-provider
-cd "c:\Users\DELL\Documents\Ubaydah's Website\TrioBazar-website\client"
-npm run build
--- start-server.bat
+++
@@ -1,6 +0,0 @@
-echo "Starting MongoDB and Server..."
-start mongod
-timeout /t 5
-cd server
-npm install
-npm start
